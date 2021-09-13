ab7dcf0acb7536b1e9f932d5fc1d6de5d1d80c24
alex-orlov <67315235+iobox420@users.noreply.github.com>
2021-09-13 17:45:19 +0500

    Add eslint in dependencies, and 'yarn run lint' in scripts

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ wss.on("connection", connection);*/
 
 async function start() {
   try {
-    console.log('Start connect to mongoDB');
+    console.log('Start connect to mongo DB');
     await mongoose.connect(MongoDbConnectionDriverCode, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
