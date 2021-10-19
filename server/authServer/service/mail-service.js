517923ec27eaac2df444dc37e4d9b6762b002c5b
alex-orlov <67315235+iobox420@users.noreply.github.com>
2021-10-19 15:57:48 +0500

    mail service uodate

--- server/authServer/service/mail-service.js
+++ server/authServer/service/mail-service.js
@@ -4,7 +4,7 @@ class MailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
@@ -13,7 +13,7 @@ class MailService {
   }
   async sendActivationMail(to, link) {
     await this.transporter.sendMail({
-      from: process.env.SMTP_USER,
+      from: process.env.SMTP_USER_SEND,
       to,
       subject: 'Активация аккаунта на ' + process.env.API_URL,
       text: '',
